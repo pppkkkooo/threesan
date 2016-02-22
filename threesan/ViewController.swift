67c87b11cffe5240f48c5f76ec4a89249de4e604
kyo <kyo@kyo-no-macbook-air.local>
2016-02-22 20:55:12 +0900

    Initial Commit

---
+++ threesan/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  threesan
+//
+//  Created by kyo on 2016/02/22.
+//  Copyright © 2016年 kyo. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
